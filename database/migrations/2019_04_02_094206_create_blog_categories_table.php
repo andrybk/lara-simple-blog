2d4bcb72258d13d32fe2df0cc19ea546e602fcf9
andrybk <and.levelupwork@gmail.com>
2019-04-02 15:31:54 +0500

    Post and categories migrations are made

--- database/migrations/2019_04_02_094206_create_blog_categories_table.php
+++ database/migrations/2019_04_02_094206_create_blog_categories_table.php
@@ -15,7 +15,15 @@ class CreateBlogCategoriesTable extends Migration
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('parent_id')->default(0);
+
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
+
         });
     }
 
