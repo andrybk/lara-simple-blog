793884c0f75d072c0bf07bc66bc06490ac432b81
andrybk <and.levelupwork@gmail.com>
2019-04-03 11:37:47 +0500

    Post controller added routes changed

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('rest', 'RestTestController')->names('restTest');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function (){
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
