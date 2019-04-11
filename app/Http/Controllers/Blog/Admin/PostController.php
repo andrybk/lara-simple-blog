53de332011c14b6c5ca168ee3de1a96b9c1fb266
andrybk <and.levelupwrok@gmail.com>
2019-04-11 06:20:25 +0500

    check

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -31,8 +31,6 @@ class PostController extends BaseController
      */
     public function index()
     {
-        //
-//
        $paginator = $this->blogPostRepository->getAllWithPaginate(25);
 
         return view('blog.admin.posts.index', compact('paginator'));
