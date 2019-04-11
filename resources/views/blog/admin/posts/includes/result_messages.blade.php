f982c5a64ba7f991c638e945d6bb7a985142623f
andrybk <and.rybk@ya.ru>
2019-04-11 20:16:27 +0500

    result messages as independent blade template

---
+++ resources/views/blog/admin/posts/includes/result_messages.blade.php
@@ -0,0 +1,27 @@
+@php /** @var \Illuminate\Support\ViewErrorBag $errors*/ @endphp
+@php /** @var Succ $errors*/ @endphp
+@if($errors ->any())
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">x</span>
+                </button>
+                {{$errors->first()}}
+            </div>
+        </div>
+    </div>
+
+@endif
+@if(session('success'))
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="alert alert-success">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">x</span>
+                </button>
+                {{session()->get('success')}}
+            </div>
+        </div>
+    </div>
+@endif
