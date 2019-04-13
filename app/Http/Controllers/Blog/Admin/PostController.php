12fda44efd0acb4823d39fcdfb0e8664ef7747bd
andrybk <and.rybk@ya.ru>
2019-04-13 18:01:06 +0500

    posts update

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Models\BlogPost;
 use App\Repositories\BlogCategoryRepository;
 use App\Repositories\BlogPostRepository;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
 
 /**
  * Class PostController
@@ -34,7 +35,7 @@ class PostController extends BaseController
      */
     public function index()
     {
-       $paginator = $this->blogPostRepository->getAllWithPaginate(25);
+        $paginator = $this->blogPostRepository->getAllWithPaginate(25);
 
         return view('blog.admin.posts.index', compact('paginator'));
     }
@@ -52,7 +53,7 @@ class PostController extends BaseController
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -63,7 +64,7 @@ class PostController extends BaseController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -74,14 +75,14 @@ class PostController extends BaseController
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         $item = $this->blogPostRepository->getEdit($id);
 
-        if(empty($item)){
+        if (empty($item)) {
             abort(404);
         }
 
@@ -95,23 +96,76 @@ class PostController extends BaseController
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+
+        if(empty($item)){
+            return back()
+                ->withErrors(['msg' => "Post id=[{$id}] not found"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+
+        if(empty($data['slug'])){
+            $data['slug'] = \Str::slug($data['title']);
+        }
+        if (empty($item->published_at) && $data['is_published']){
+            $data['published_at'] = Carbon::now();
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.posts.edit', $item->id)
+                ->with(['success' => 'Successfully saved']);
+        }
+        else
+        {
+            return back()
+                ->withErrors(['msg' => 'Save error'])
+                ->withInput();
+        }
+
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        //TODO:SoftDelete
+        $item = BlogPost::find($id);
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Post id=[{$id}] not found"])
+                ->withInput();
+        }
+        $claim_remove_result = $item
+            ->delete();
+        foreach ($item->uploads as $upload) {
+            $upload
+                ->delete();
+        }
+        if ($claim_remove_result) {
+            return redirect()
+                ->route('blog.admin.posts.index', $item->id)
+                ->with(['success' => 'Successfully deleted']);
+        } else {
+            return back()
+                ->withErrors(['msg' => "Deletion error"])
+                ->withInput();
+        }
+
     }
 }
--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,15 +5,54 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class BlogPost
+ *
+ * @package App\Models
+ *
+ * @property BlogCategory   $category
+ * @property User           $user
+ * @property string         $title
+ * @property string         $slug
+ * @property string         $content_html
+ * @property string         $content_raw
+ * @property string         $except
+ * @property boolean        $is_published
+ * @property string         $published_att
+ *
+ */
+
+
 class BlogPost extends Model
 {
     //
     use SoftDeletes;
 
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'excerpt',
+        'content_raw',
+        'is_published',
+        'published_at',
+        'user_id'
+    ];
+
+    /**
+     * Posts category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category(){
         return $this->belongsTo(BlogCategory::class);
     }
 
+    /**
+     * Author of post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
